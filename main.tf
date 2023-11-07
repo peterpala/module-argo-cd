c5de7b8df326ec579cd38201e7f81589d11eddc7
Peter Pala <peter.pala@disroot.org>
2023-11-07 20:09:25 +0100

    changed v1alpha1 to v1beta1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
@@ -15,7 +15,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     }
@@ -37,7 +37,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
   host                   = data.aws_eks_cluster.msur.endpoint
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
   }
@@ -51,7 +51,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
     host                   = data.aws_eks_cluster.msur.endpoint
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
     }
